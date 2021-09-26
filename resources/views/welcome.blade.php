0567c8076d0ff0a820333ed4b33484a9b2453fbc
checodaniel04 <checo04@gmail.com>
2021-09-26 17:07:32 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,14 +43,7 @@
               
             </div>                      
           </div>
-          <div class="hidden sm:block sm:ml-6">
-            <div class="flex space-x-4">
-              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Login</a>
-  
-              
-            </div>                      
-          </div>
+          
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           
@@ -78,6 +71,22 @@
       <div class="px-2 pt-2 pb-3 space-y-1">
         
         <a href="/" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Bienvenidos</a>
+  
+        
+      </div>
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+                <a href="{{ url('/dashboard') }}" class="text-sm  underline text-white">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="text-sm underline text-white">Log in</a>
+
+                
+            @endauth
+        </div>
+    @endif
   
         
       </div>
